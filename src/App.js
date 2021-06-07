0999a13e179ae7f2346f9f89380e84a5b68646f6
Henry Doan <henrydoan96@gmail.com>
2021-06-06 21:12:32 -0600

    working project

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { Header, Form, Item, Container, Grid } from 'semantic-ui-react'
+import axios from 'axios';
+
+const App = () => {
+  const [stock, setStock] = useState("")
+  const [stockInfo, setStockInfo] = useState([])
+
+  const handleSubmit = () => {
+    axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=`)
+      .then( res => {
+        setStockInfo(Object.values(res.data)[0])
+      })
+      .catch( err => alert(err))
+  }
+  
+  const stockInfoDetails = Object.entries(stockInfo).map(([key,value]) => {
+    return (
+      <Item>
+        <Item.Content verticalAlign='middle'>
+          {key} : {value.toString()}
+        </Item.Content>
+      </Item>
+    );
+  })
+  return(
+    <Container>
+      <Header as="h1" textAlign='center'>Stock App</Header>
+      <Grid columns={2} divided>
+        <Grid.Row>
+          <Grid.Column>
+          <Form onSubmit={handleSubmit}>
+            <Form.Input
+              name='stock'
+              value={stock}
+              onChange={(e) => setStock(e.target.value)}
+              required
+              label="Search up your stock:"
+            />
+            <Form.Button>Submit</Form.Button>
+          </Form>
+          </Grid.Column>
+          <Grid.Column>
+          <Item.Group divided>
+            { stockInfoDetails }
+          </Item.Group>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+  )
+}
+
+export default App;
